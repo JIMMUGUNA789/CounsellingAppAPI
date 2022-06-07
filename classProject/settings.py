d3995b03d6550d81b0bc40403b081369f4c3b7e6
JIMMUGUNA789 <jimmuguna@gmail.com>
2022-06-07 01:23:30 +0000

    django_heroku configured

--- classProject/settings.py
+++ classProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +142,5 @@ MEDIA_URL = '/media/'
       
     # Path where media is stored  
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  
+
+django_heroku.settings(locals())
